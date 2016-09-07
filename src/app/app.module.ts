013c4e4edcc774104a7f9c25c0d1e580a2b7d1bc
Candice Humpherys <smrtsmrf@gmail.com>
2016-09-07 10:36:34 -0600

    committing all of candice's changes cause it won't let me push..

---
+++ src/app/app.module.ts
@@ -0,0 +1,31 @@
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { Sidebar } from './sidebar/sidebar.component';
+import { LocationHeader } from './location-header/location-header.component';
+import { ScheduleHeader } from './schedule-header/schedule-header.component';
+import { CalendarHeader } from './calendar-header/calendar-header.component';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    Sidebar,
+    LocationHeader,
+    ScheduleHeader,
+    CalendarHeader
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    HttpModule
+  ],
+  providers: [
+    
+    ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }

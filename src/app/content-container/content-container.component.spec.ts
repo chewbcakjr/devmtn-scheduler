9ce604e62bbf9bfdb99642a1fc5c0bf3b2771a02
aleeex <akang2@gmail.com>
2016-09-08 12:04:19 -0600

    ran ng g content-container to create new component

---
+++ src/app/content-container/content-container.component.spec.ts
@@ -0,0 +1,11 @@
+/* tslint:disable:no-unused-variable */
+
+import { TestBed, async } from '@angular/core/testing';
+import { ContentContainerComponent } from './content-container.component';
+
+describe('Component: ContentContainer', () => {
+  it('should create an instance', () => {
+    let component = new ContentContainerComponent();
+    expect(component).toBeTruthy();
+  });
+});

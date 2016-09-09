f53aafaaa857a2630ced2067116e2a4068765bde
Candice Humpherys <smrtsmrf@gmail.com>
2016-09-08 22:48:22 -0600

    updated .gitignore; finalized server.js file (hard coded version); gave up on gulp

--- src/server/server.js
+++ src/server/server.js
@@ -1,40 +1,41 @@
-import express from 'express';
-import bodyParser from 'body-parser'
-import cors from 'cors';
-import session from 'express-session';
-import passport from 'passport';
-import gcal from 'google-calendar';
-import util from 'util';
-import config from './config';
+var express = require('express');
+var bodyParser = require('body-parser');
+var cors = require('cors');
+var session = require('express-session');
+var passport = require('passport');
+var gcal = require('google-calendar');
+// var util = require('util');
+var config = require('./config');
+var google = require('googleapis');
 
-const GoogleStrategy = require('passport-google-oauth20').Strategy;
+var GoogleStrategy = require('passport-google-oauth20').Strategy;
 
-import google from 'googleapis';
-const calendar = google.calendar('v3');
-const tasks = google.tasks('v1');
 
-let OAuth2 = google.auth.OAuth2;
-let oauth2Client = new OAuth2(config.consumer_key, config.consumer_secret, '/auth/google/callback');
+var calendar = google.calendar('v3');
+var tasks = google.tasks('v1');
+
+var OAuth2 = google.auth.OAuth2;
+var oauth2Client = new OAuth2(config.consumer_key, config.consumer_secret, '/auth/google/callback');
 google.options({auth: oauth2Client}); // set auth as a global default
 
 //App Init
-const app = express();
-const port = 9001;
+var app = express();
+var port = 9001;
 
 //Middleware
 app.use(bodyParser.json());
 var corsOptions = {
 	origin: 'http://localhost:9001'
-}
+};
 app.use(cors(corsOptions));
 app.use(express.static(__dirname + '/../../../build')); //serve all of our static front-end files from our server.
-app.use(session({secret: config.session_secret, resave: true, saveUninitialized: true}));
+app.use(session({ secret: config.session_secret, resave: true, saveUninitialized: true }));
 
 //Passport
-passport.serializeUser(function(user, cb) {
+passport.serializeUser(function (user, cb) {
 	cb(null, user);
 });
-passport.deserializeUser(function(obj, cb) {
+passport.deserializeUser(function (obj, cb) {
 	cb(null, obj);
 });
 app.use(passport.initialize());
@@ -45,13 +46,13 @@ passport.use(new GoogleStrategy({
 	clientSecret: config.consumer_secret,
 	callbackURL: "http://localhost:" + port + "/auth/google/callback",
 	scope: ['openid', 'email', 'https://www.googleapis.com/auth/calendar', 'https://www.googleapis.com/auth/tasks']
-}, (accessToken, refreshToken, profile, done) => {
+}, function (accessToken, refreshToken, profile, done) {
 
 	console.log("Auth Success. Celebration is in order.");
 
 	console.log("Access Token: ", accessToken);
 
-	oauth2Client.setCredentials({access_token: accessToken, refresh_token: refreshToken});
+	oauth2Client.setCredentials({ access_token: accessToken, refresh_token: refreshToken });
 
 	return done(null, profile);
 }));
@@ -59,50 +60,48 @@ passport.use(new GoogleStrategy({
 //Auth Routing
 app.get('/auth/google', passport.authenticate('google'));
 
-app.get('/auth/google/callback', passport.authenticate('google', {failureRedirect: '/'}), (req, res) => {
+app.get('/auth/google/callback', passport.authenticate('google', { failureRedirect: '/' }), function (req, res) {
 	// Successful authentication, redirect home.
 	res.redirect('/');
 });
 
 // GET A LIST OF THE USER'S CALENDARS
-app.get('/calendars', function(req, res, next) {
+app.get('/calendars', function (req, res, next) {
 	calendar.calendarList.list({
-		auth: oauth2Client,
-	},function(err, calendars) {
+		auth: oauth2Client
+	}, function (err, calendars) {
 		if (err) {
-			console.log('error returning user\'s calendars: ' + err)
-			res.send('err')
+			console.log('error returning user\'s calendars: ' + err);
+			res.send('err');
 		} else {
-			res.send(calendars)
+			res.send(calendars);
 		}
-	})
-})
+	});
+});
 
 // CREATE A NEW CALENDAR
-app.post('/calendars', (req, res) => {
-	let newCalendar = {
+app.post('/calendars', function (req, res) {
+	var newCalendar = {
 		// summary: 'DevMountain Cohort Schedule 3'
 		summary: 'New Calendar'
 	};
 
-	calendar
-		.calendars
-		.insert({
-			auth: oauth2Client,
-			resource: newCalendar
-		}, function(err, calendar) {
-			if (err) {
-				console.log('There was an error contacting the Calendar service: ' + err);
-				// return;
-				res.send('err')
-			}
-			res.send('Calendar created: %s', util.inspect(calendar));
-		});
+	calendar.calendars.insert({
+		auth: oauth2Client,
+		resource: newCalendar
+	}, function (err, calendar) {
+		if (err) {
+			console.log('There was an error contacting the Calendar service: ' + err);
+			// return;
+			res.send('err');
+		}
+		res.send('Calendar created: %s', calendar);
+	});
 });
 
 // CREATE A NEW EVENT
-app.post('/events', (req, res, next) => {
-	let newEvent = {
+app.post('/events', function (req, res, next) {
+	var newEvent = {
 		'summary': 'Event created via nodemon and postman',
 		'location': '560 S 100 W St, Provo, UT 84601',
 		'description': 'Ta Da!!!',
@@ -117,135 +116,115 @@ app.post('/events', (req, res, next) => {
 		'recurrence': [
 			// 'RRULE:FREQ=WEEKLY;COUNT=1'
 		],
-		'attendees': [
-			{
-				'email': 'mrevanwitt@gmail.com'
-			}, {
-				'email': 'akang2@gmail.com'
-			}
-			// , {
-			// 	'email': 'jeremy@devmounta.in'
-			// }
-			// , {
-			// 	'email': 'jeremy@devmounta.in'
-			// }
-			// , {
-			// 	'email': 'jeremy@devmounta.in'
-			// }
+		'attendees': [{
+			'email': 'mrevanwitt@gmail.com'
+		}, {
+			'email': 'akang2@gmail.com'
+		}
+		// , {
+		// 	'email': 'jeremy@devmounta.in'
+		// }
+		// , {
+		// 	'email': 'jeremy@devmounta.in'
+		// }
+		// , {
+		// 	'email': 'jeremy@devmounta.in'
+		// }
 		],
 		'reminders': {
 			'useDefault': false,
-			'overrides': [
-				{
-					'method': 'email',
-					'minutes': 24 * 60
-				}
-			]
-		}, 
+			'overrides': [{
+				'method': 'email',
+				'minutes': 24 * 60
+			}]
+		},
 		sequence: 1
 	};
 
-	calendar
-		.events
-		.insert({
-			auth: oauth2Client,
-			calendarId: '3luc0md0o7gmrscugapr5lvn2g@group.calendar.google.com',
-			resource: newEvent,
-			sendNotifications: false
-		}, function(err, event) {
-			if (err) {
-				console.log('There was an error contacting the Calendar service: ' + err);
-				// return;
-				res.send(err)
-			} else {
-				res.send('Event created: %s', event.htmlLink);
-				// res.redirect('/');
-			}
-		});
+	calendar.events.insert({
+		auth: oauth2Client,
+		calendarId: '3luc0md0o7gmrscugapr5lvn2g@group.calendar.google.com',
+		resource: newEvent,
+		sendNotifications: false
+	}, function (err, event) {
+		if (err) {
+			console.log('There was an error contacting the Calendar service: ' + err);
+			// return;
+			res.send(err);
+		} else {
+			res.send('Event created: %s', event.htmlLink);
+			// res.redirect('/');
+		}
+	});
 });
 
 // GET A LIST OF THE USER'S TASK LISTS
-app.get('/tasklists', function(req, res, next) {
+app.get('/tasklists', function (req, res, next) {
 	tasks.tasklists.list({
-		auth: oauth2Client,
-	},function(err, tasklists) {
+		auth: oauth2Client
+	}, function (err, tasklists) {
 		if (err) {
-			res.send('error returning user\'s task lists: ' + err)
+			res.send('error returning user\'s task lists: ' + err);
 		} else {
-			res.send(tasklists)
+			res.send(tasklists);
 		}
-	})
-})
+	});
+});
 
 // CREATE NEW TASK LIST
-app.post('/tasklists', function(req, res, next) {
-	let newTaskList = {
+app.post('/tasklists', function (req, res, next) {
+	var newTaskList = {
 		title: 'New Task List'
 	};
 
 	tasks.tasklists.insert({
 		auth: oauth2Client,
 		resource: newTaskList
-	},function(err, tasklist) {
+	}, function (err, tasklist) {
 		if (err) {
-			res.send('error creating new task list: ' + err)
+			res.send('error creating new task list: ' + err);
 		} else {
-			res.send(tasklist)
+			res.send(tasklist);
 		}
-	})
-})
+	});
+});
 
 // GET A LIST OF TASKS
-app.get('/tasks', function(req, res, next) {
+app.get('/tasks', function (req, res, next) {
 	tasks.tasks.list({
 		auth: oauth2Client,
 		tasklist: 'MTU0OTY5Njk1NTM4NzgwNjY5MDg6MDow'
-	},function(err, tasks) {
+	}, function (err, tasks) {
 		if (err) {
-			console.log('error returning task list: ' + err)
-			res.send('err')
+			console.log('error returning task list: ' + err);
+			res.send('err');
 		} else {
-			res.send(tasks)
+			res.send(tasks);
 		}
-	})
-})
+	});
+});
 
 // CREATE NEW TASK 
-app.post('/tasks', function(req, res, next) {
-	let newTask = {
-		
+app.post('/tasks', function (req, res, next) {
+	var newTask = {
+
 		title: 'New Task'
 	};
 
 	tasks.tasks.insert({
 		auth: oauth2Client,
-		tasklist: 'MTU0OTY5Njk1NTM4NzgwNjY5MDg6MDow', 
+		tasklist: 'MTU0OTY5Njk1NTM4NzgwNjY5MDg6MDow',
 		resource: newTask
-	},function(err, task) {
+	}, function (err, task) {
 		if (err) {
-			res.send('error creating new task list: ' + err)
+			res.send('error creating new task list: ' + err);
 		} else {
-			res.send(task)
+			res.send(task);
 		}
-	})
-})
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+	});
+});
 
 //Listen
-app.listen(port, () => {
+app.listen(port, function () {
 	console.log('Listening on port ', port);
-})
+});

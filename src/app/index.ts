eb43bdb9756e3357a8a34f0dced188c7b48a5ace
aleeex <akang2@gmail.com>
2016-09-09 18:01:38 -0600

    from the index.ts file, turns out we dont need the exports * at the top. commented out because it doesn't work. yet. candice wants to refactor later

--- src/app/index.ts
+++ src/app/index.ts
@@ -1,8 +1,8 @@
-export * from './sidebar/sidebar.component';
-export * from './location-header/location-header.component';
-export * from './schedule-header/schedule-header.component';
-export * from './calendar-header/calendar-header.component';
-export * from './content-container/content-container.component';
+// export * from './sidebar/sidebar.component';
+// export * from './location-header/location-header.component';
+// export * from './schedule-header/schedule-header.component';
+// export * from './calendar-header/calendar-header.component';
+// export * from './content-container/content-container.component';
 
 export * from './app.component';
 export * from './app.module';

85cf33aad3bf568d79aa311c5826d2bce5616214
Candice Humpherys <smrtsmrf@gmail.com>
2016-09-12 11:29:00 -0600

    add calendar header title component

---
+++ src/app/calendar-header-title/calendar-header-title.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-calendar-header-title',
+  templateUrl: './calendar-header-title.component.html',
+  styleUrls: ['./calendar-header-title.component.scss']
+})
+export class CalendarHeaderTitleComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}

2d8f8fef9751a1ace2890996b11ee419a5cac74c
Catherine Marston <catherine@thesnowflakeapp.com>
2016-09-13 17:52:15 -0600

    changed forms

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import { TaskComponent } from './task/task.component';
 import { CalendarHeaderTitleComponent } from './calendar-header-title/calendar-header-title.component';
 import { EventCardComponent } from './event-card/event-card.component';
 import { FooterComponent } from './footer/footer.component';
+import { TemplateListComponent } from './template-list/template-list.component';
 
 
 
@@ -29,7 +30,8 @@ import { FooterComponent } from './footer/footer.component';
     TaskComponent,
     CalendarHeaderTitleComponent,
     EventCardComponent,
-    FooterComponent
+    FooterComponent,
+    TemplateListComponent
   ],
   imports: [
     BrowserModule,

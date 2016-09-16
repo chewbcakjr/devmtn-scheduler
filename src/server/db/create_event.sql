828bd18a35730c8a7e2c6d3150cb159961fc82a5
Candice Humpherys <smrtsmrf@gmail.com>
2016-09-16 15:58:34 -0600

    create event function now returns that event and pushes it on to the events property of the service. also had to create a go live link in the side bar

--- src/server/db/create_event.sql
+++ src/server/db/create_event.sql
@@ -1,3 +1,4 @@
 insert into event
 	(template_id, name, start_time, end_time, default_instructor, notes, day_number)
 	values ($1, $2, $3, $4, $5, $6, $7)
+	returning template_id, name, start_time, end_time, default_instructor, notes, day_number;

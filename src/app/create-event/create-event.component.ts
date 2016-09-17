aa64316e6b6a05dd687eb22a286e9f353c0901cd
Catherine Marston <catherine@thesnowflakeapp.com>
2016-09-17 10:57:18 -0600

    added create event template and create event form

--- src/app/create-event/create-event.component.ts
+++ src/app/create-event/create-event.component.ts
@@ -1,15 +1,25 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
 import { EventsService } from '../events.service';
 import { TemplatesService } from '../templates.service';
-
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 @Component({
   selector: 'app-create-event',
   templateUrl: './create-event.component.html',
   styleUrls: ['./create-event.component.scss']
 })
 export class CreateEventComponent implements OnInit {
+	form: FormGroup;
 
-  constructor(private eventsService:EventsService, private templatesService:TemplatesService) { }
+  constructor(private eventsService:EventsService, private templatesService:TemplatesService, fb:FormBuilder) {
+		this.form = fb.group({
+			      name: ['', Validators.required],
+			      day_number: [''],
+						start_time: [''],
+						end_time: [''],
+			      default_instructor: [''],
+						notes: ['']
+					})
+				}
 
   ngOnInit():void {
     console.log(this.templatesService.currTmpl);
--- src/app/create-template/create-template.component.ts
+++ src/app/create-template/create-template.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { TemplatesService } from '../templates.service';
+import { Router, RouterModule } from '@angular/router';
+import { FormControl, FormBuilder, FormGroup, Validators }  from '@angular/forms';
 
 @Component({
   selector: 'app-create-template',
@@ -7,9 +10,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CreateTemplateComponent implements OnInit {
 
-  constructor() { }
+  constructor(fb: FormBuilder, private templatesService: TemplatesService, private router: Router) { }
 
   ngOnInit() {
   }
-
+	createTmpl(name:string) {
+  	this.templatesService.createTmpl(name)
+  		// .subscribe(data => {
+  			// console.log(data);
+  			// this.newTmpl = data;
+  		// })
+  }
 }

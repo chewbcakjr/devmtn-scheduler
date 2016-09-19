2952e62087971a81758472799751e75872a428e9
Candice Humpherys <smrtsmrf@gmail.com>
2016-09-19 12:59:11 -0600

    add a redirect, also display template title dynamically on template view

--- src/app/calendar-header/calendar-header.component.ts
+++ src/app/calendar-header/calendar-header.component.ts
@@ -1,14 +1,20 @@
 import { Component, OnInit } from '@angular/core';
+import { TemplatesService } from '../templates.service';
 
 @Component({
   selector: 'app-calendar-header',
   templateUrl: './calendar-header.component.html',
   styleUrls: ['./calendar-header.component.scss']
 })
-export class CalendarHeader {
+export class CalendarHeader implements OnInit {
 
-  constructor() { }
+  constructor(private templatesService:TemplatesService) { }
 
+  currTmpl = {};
+  currWeek = 'Week One'
 
+  ngOnInit() {
+  	this.currTmpl = this.templatesService.currTmpl;
+  }
 
 }

eb8e89e693a59e7e8e02a62765b4129ccec28bc6
mrevanwitt <mrevanwitt@gmail.com>
2016-09-20 17:29:17 -0600

    move logic from component to service, and restructure data so we can use the week functionality (display one week at a time)

--- src/app/events.service.ts
+++ src/app/events.service.ts
@@ -3,21 +3,66 @@ import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { Http, Response } from '@angular/http';
 import 'rxjs/add/operator/map';
+import { Router } from '@angular/router';
 
 @Injectable()
 export class EventsService {
 
-  constructor(private http:Http) { }
+  constructor(private http:Http, private router:Router) { }
   events:any[] = [];
   base_url:string = 'http://localhost:9001';
-
+  week = [];
+  weeks = [];
+  count = 0;
+  eventsGrouped = [];
   // return a list of events associated with a specified template.
   getEvents(tmpl_id:number):Observable<any> {
+    this.weeks = [];
+
+
   	return this.http.get(`${this.base_url}/dbevents?tmpl_id=${tmpl_id}`)
   		.map(res => {
                 this.events = res.json();
-                return res.json()
+                // console.log(this.events)
+                let lastDay = this.events[this.events.length-1].day_number;
+                for (let i = 1; i <= lastDay; i++) {
+                  // console.log('i',i)
+          				let mult = this.events.filter(event => event.day_number == i)
+                  // console.log('mult',mult)
+          				if (mult.length > 0) {
+          					this.eventsGrouped.push(mult)
+          				}
+          			}
+                // console.log(this.eventsGrouped.length)
+                // console.log(this.eventsGrouped)
+                let maxWeek = Math.ceil(this.eventsGrouped[this.eventsGrouped.length - 1][0].day_number / 7);
+                // console.log(maxWeek)
+                for (let weekNum = 1; weekNum <= maxWeek; weekNum++) {
+                  let temp = this.eventsGrouped.map(day => {
+                    return day.filter(event => {
+                      return Math.ceil(event.day_number / 7) == weekNum;
+                    });
+                  });
+                  // console.log(temp)
+                  this.weeks[weekNum - 1] = temp.filter(item => {
+                    return item.length > 0;
+                  });
+                }
+                this.week = this.weeks[this.count];
+                // console.log(this.weeks)
+                return this.week
               })
+  }
+  increment() {
+    this.count++;
+    // console.log(this.count)
+    this.week = this.weeks[this.count];
+    this.router.navigate(['template',this.count+1])
+  }
+  decrement() {
+    this.count--;
+    this.week = this.weeks[this.count];
+    this.router.navigate(['template',this.count+1])
   }
 
   // create new event on given template

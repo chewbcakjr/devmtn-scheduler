f9e6647cddfae67b76d01afeaead44226d74e6a7
mrevanwitt <mrevanwitt@gmail.com>
2016-09-19 18:00:55 -0600

    Able to now pull events dynamically

--- src/app/template/template.component.ts
+++ src/app/template/template.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { TemplatesService } from '../templates.service';
+import { EventsService } from '../events.service';
 
 @Component({
   selector: 'app-template',
@@ -8,10 +9,20 @@ import { TemplatesService } from '../templates.service';
 })
 export class TemplateComponent implements OnInit {
 
-  constructor(private templatesService:TemplatesService) { }
+  constructor(private templatesService:TemplatesService, private eventsService:EventsService) { }
+
+  events = [];
 
   ngOnInit() {
-  	console.log(this.templatesService.currTmpl)
+  	console.log(this.templatesService.currTmpl.template_id)
+    this.eventsService.getEvents(this.templatesService.currTmpl.template_id).subscribe(data => {
+      for (let i = 1; i <= data.length; i++) {
+        let mult = data.filter(event => event.day_number == i)
+        if (mult.length > 0) {
+          this.events.push(mult)
+        }
+      }
+      console.log('data', this.events)
+    })
   }
-
 }

efc81e0865441aba84fbbca600a5f9053be2859c
mrevanwitt <mrevanwitt@gmail.com>
2016-09-20 17:26:49 -0600

    added week display name dynamically, and functionality for scrolling through weeks

--- src/app/calendar-header/calendar-header.component.ts
+++ src/app/calendar-header/calendar-header.component.ts
@@ -1,20 +1,73 @@
 import { Component, OnInit } from '@angular/core';
 import { TemplatesService } from '../templates.service';
+import { TemplateComponent } from '../template/template.component'
+import { EventsService } from '../events.service';
 
 @Component({
-  selector: 'app-calendar-header',
-  templateUrl: './calendar-header.component.html',
-  styleUrls: ['./calendar-header.component.scss']
+	selector: 'app-calendar-header',
+	templateUrl: './calendar-header.component.html',
+	styleUrls: ['./calendar-header.component.scss']
 })
 export class CalendarHeader implements OnInit {
 
-  constructor(private templatesService:TemplatesService) { }
+	constructor(private templatesService: TemplatesService, private templateComponent: TemplateComponent, private eventsService: EventsService) { }
 
-  currTmpl = {};
-  currWeek = 'Week One'
+	currTmpl = {};
 
-  ngOnInit() {
-  	this.currTmpl = this.templatesService.currTmpl;
-  }
+	weeks = [
+		'ONE',
+		'TWO',
+		'THREE',
+		'FOUR',
+		'FIVE',
+		'SIX',
+		'SEVEN',
+		'EIGHT',
+		'NINE',
+		'TEN',
+		'ELEVEN',
+		'TWELVE',
+		'THIRTEEN',
+		'FOURTEEN',
+		'FIFTEEN',
+		'SIXTEEN'
+	];
+
+
+	currWeek = this.weeks[0];
+
+	ngOnInit() {
+		this.currTmpl = this.templatesService.currTmpl;
+	}
+
+	nextWeek() {
+		// console.log('nextWeek')
+		for (var i = 0; i <= this.weeks.length; i++) {
+			if (this.weeks[i] === this.currWeek) {
+				if (this.currWeek === this.weeks[0] + (this.weeks.length - 1)) {
+					this.currWeek = this.weeks[0] + this.weeks.length;
+				} else {
+					this.currWeek = this.weeks[i + 1]
+					break;
+				}
+			}
+		}
+    this.eventsService.increment();
+
+	}
+	lastWeek() {
+		// console.log('lastWeek')
+		for (var i = 0; i <= this.weeks.length; i++) {
+			if (this.weeks[i] === this.currWeek) {
+				if (this.currWeek === this.weeks[0]) {
+					this.currWeek = this.weeks[0]
+				} else {
+					this.currWeek = this.weeks[i - 1]
+					break;
+				}
+			}
+		}
+    this.eventsService.decrement();
+	}
 
 }

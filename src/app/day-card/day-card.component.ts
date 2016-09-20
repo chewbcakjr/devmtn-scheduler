bb3dd87c7593ca6313b60ba42e7149211076b4ce
Catherine Marston <catherine@thesnowflakeapp.com>
2016-09-20 08:03:51 -0600

    added create event to new button

--- src/app/day-card/day-card.component.ts
+++ src/app/day-card/day-card.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { EventsService } from '../'
 
 @Component({
   selector: 'app-day-card',
@@ -11,5 +12,12 @@ export class DayCardComponent implements OnInit {
 
   ngOnInit() {
   }
+
+	createEvent(tmpl_id:number, name:string, start_time:string, end_time:string, default_instructor:string, notes:string, day_number:number) {
+		this.eventsService.createEvent(this.templatesService.currTmpl.template_id, name, start_time, end_time, default_instructor, notes, day_number)
+			.subscribe(data => {
+				console.log(data);
+			})
+	}
 
 }

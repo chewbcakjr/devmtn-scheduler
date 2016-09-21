a8d41c94d83880041e0b86f1d51b62c48e5a6dc3
Candice Humpherys <smrtsmrf@gmail.com>
2016-09-20 22:44:27 -0600

    dynamically update week number display based on route params

--- src/app/calendar-header/calendar-header.component.ts
+++ src/app/calendar-header/calendar-header.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { TemplatesService } from '../templates.service';
 import { EventsService } from '../events.service';
+import { ActivatedRoute, Params } from  '@angular/router';
 
 @Component({
 	selector: 'app-calendar-header',
@@ -9,63 +10,68 @@ import { EventsService } from '../events.service';
 })
 export class CalendarHeader implements OnInit {
 
-	constructor(private templatesService: TemplatesService, private eventsService: EventsService) { }
+	constructor(private templatesService: TemplatesService, private eventsService: EventsService, private route:ActivatedRoute) { }
 
 	currTmpl = {};
 
-	weeks = [
-		'ONE',
-		'TWO',
-		'THREE',
-		'FOUR',
-		'FIVE',
-		'SIX',
-		'SEVEN',
-		'EIGHT',
-		'NINE',
-		'TEN',
-		'ELEVEN',
-		'TWELVE',
-		'THIRTEEN',
-		'FOURTEEN',
-		'FIFTEEN',
-		'SIXTEEN'
-	];
+	// weeks = [
+	// 	'ONE',
+	// 	'TWO',
+	// 	'THREE',
+	// 	'FOUR',
+	// 	'FIVE',
+	// 	'SIX',
+	// 	'SEVEN',
+	// 	'EIGHT',
+	// 	'NINE',
+	// 	'TEN',
+	// 	'ELEVEN',
+	// 	'TWELVE',
+	// 	'THIRTEEN',
+	// 	'FOURTEEN',
+	// 	'FIFTEEN',
+	// 	'SIXTEEN'
+	// ];
 
 
-	currWeek = this.weeks[0];
+	// currWeek = this.weeks[0];
+
 
 	ngOnInit() {
 		this.currTmpl = this.templatesService.currTmpl;
+		this.route.params.forEach((params: Params) => {
+      	this.currWeek = +params['weeknum'];
+    })
 	}
 
 	nextWeek() {
 		// console.log('nextWeek')
-		for (var i = 0; i <= this.weeks.length; i++) {
-			if (this.weeks[i] === this.currWeek) {
-				if (this.currWeek === this.weeks[0] + (this.weeks.length - 1)) {
-					this.currWeek = this.weeks[0] + this.weeks.length;
-				} else {
-					this.currWeek = this.weeks[i + 1]
-					break;
-				}
-			}
-		}
+		// for (var i = 0; i <= this.weeks.length; i++) {
+		// 	if (this.weeks[i] === this.currWeek) {
+		// 		if (this.currWeek === this.weeks[0] + (this.weeks.length - 1)) {
+		// 			this.currWeek = this.weeks[0] + this.weeks.length;
+		// 		} else {
+		// 			this.currWeek = this.weeks[i + 1]
+		// 			break;
+		// 		}
+		// 	}
+		// }
+		
     this.eventsService.increment();
 
 	}
 	lastWeek() {
 		// console.log('lastWeek')
-		for (var i = 0; i <= this.weeks.length; i++) {
-			if (this.weeks[i] === this.currWeek) {
-				if (this.currWeek === this.weeks[0]) {
-					this.currWeek = this.weeks[0]
-				} else {
-					this.currWeek = this.weeks[i - 1]
-					break;
-				}
-			}
-		}
+		// for (var i = 0; i <= this.weeks.length; i++) {
+		// 	if (this.weeks[i] === this.currWeek) {
+		// 		if (this.currWeek === this.weeks[0]) {
+		// 			this.currWeek = this.weeks[0]
+		// 		} else {
+		// 			this.currWeek = this.weeks[i - 1]
+		// 			break;
+		// 		}
+		// 	}
+		// }
     this.eventsService.decrement();
 	}
 

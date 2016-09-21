4cd86c7e8045c136e7edd656e74e5d5903c5a5b7
Candice Humpherys <smrtsmrf@gmail.com>
2016-09-21 09:11:36 -0600

    can't get default values to work properly, so I just removed them. But updating an event works. just need to figure out how to close the modal and redirect

--- src/app/edit-event/edit-event.component.ts
+++ src/app/edit-event/edit-event.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, OnDestroy } from '@angular/core';
 import { EventsService } from '../events.service';
 import { Router } from '@angular/router';
 
@@ -7,13 +7,13 @@ import { Router } from '@angular/router';
   templateUrl: './edit-event.component.html',
   styleUrls: ['./edit-event.component.scss']
 })
-export class EditEventComponent implements OnInit {
+export class EditEventComponent implements OnInit, OnDestroy {
   @Input() item = {};
 
   constructor(private eventsService:EventsService, private router:Router) { }
 
   ngOnInit() {
-    this.item = {};
+    
   	// this.eventsService.updateEvent(72,'updating event?', 'yesterday', 'today', 'me','notes here',2)
   	// 	.subscribe(() => {
   	// 		console.log('redirect will happen here')
@@ -22,4 +22,18 @@ export class EditEventComponent implements OnInit {
   		// })
   }
 
+  updateEvent(event_id:number, name:string, start_time:string, end_time:string, default_instructor:string, notes:string, day_number:number) {
+    console.log(arguments)
+    let weekNum = Math.ceil(day_number/7);
+    this.eventsService.updateEvent(event_id, name, start_time, end_time, default_instructor, notes, day_number)
+      .subscribe(() => {
+        console.log('updated')
+        this.router.navigate(['/template', weekNum])
+      })
+  }
+
+  ngOnDestroy() {
+    console.log('destroyed')
+    this.item = {};
+  }
 }

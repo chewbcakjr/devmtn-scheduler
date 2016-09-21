f79f26d47fb4863a7111d54046bb25ad412a8469
Candice Humpherys <smrtsmrf@gmail.com>
2016-09-21 08:49:41 -0600

    working on getting edit btn to pop up modal with default values. I'm getting close I think. Copied CreateEvent component onto EditEvent and tweaked with an input

--- src/app/edit-event/edit-event.component.ts
+++ src/app/edit-event/edit-event.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 import { EventsService } from '../events.service';
 import { Router } from '@angular/router';
 
@@ -8,16 +8,18 @@ import { Router } from '@angular/router';
   styleUrls: ['./edit-event.component.scss']
 })
 export class EditEventComponent implements OnInit {
+  @Input() item = {};
 
   constructor(private eventsService:EventsService, private router:Router) { }
 
   ngOnInit() {
-  	this.eventsService.updateEvent(72,'updating event?', 'yesterday', 'today', 'me','notes here',2)
-  		.subscribe(() => {
-  			console.log('redirect will happen here')
+    this.item = {};
+  	// this.eventsService.updateEvent(72,'updating event?', 'yesterday', 'today', 'me','notes here',2)
+  	// 	.subscribe(() => {
+  	// 		console.log('redirect will happen here')
   			// here's the logic to redirect. when we move it out of ngOnInit, uncomment this.
   			// this.router.navigate(['/template'])
-  		})
+  		// })
   }
 
 }

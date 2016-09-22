abd9d2aaefad1ac45b6df9ccd143f4797d4c5668
Candice Humpherys <smrtsmrf@gmail.com>
2016-09-22 00:02:19 -0600

    done messing with the stupid timepicker thing. it works

--- src/app/create-event/create-event.component.ts
+++ src/app/create-event/create-event.component.ts
@@ -15,8 +15,6 @@ export class CreateEventComponent implements OnInit {
   constructor(private eventsService:EventsService, private templatesService:TemplatesService, private router:Router) { }
 
   createEvent(title:string, start:string, end:string, instructor:string, links:string, day:number) {
-    start = start.concat(":00");
-    end = end.concat(":00");
     let weekNum = Math.ceil(day/7);
     this. eventsService.createEvent(this.templatesService.currTmpl.template_id, title, start, end, instructor, links, day)
       .subscribe(data => {
@@ -28,14 +26,14 @@ export class CreateEventComponent implements OnInit {
 
   ngAfterViewInit() {
     $('#timepicker-start').pickatime({
-      default: '12:00',
+      default: '10:00',
       autoclose: true,
-      twelvehour: false
+      twelvehour: false,
     });
     $('#timepicker-end').pickatime({
-      default: '12:00',
+      default: '12:30',
       autoclose: true,
-      twelvehour: false
+      twelvehour: false,
     });
     $('.timepicker-start').on("hover", function() {
       console.log($(this).text());
--- src/app/edit-event/edit-event.component.ts
+++ src/app/edit-event/edit-event.component.ts
@@ -28,45 +28,33 @@ export class EditEventComponent implements OnInit, OnDestroy {
     this.currEvent = this.eventsService.currEvent;
     console.log(this.item)
     
-  	// this.eventsService.updateEvent(72,'updating event?', 'yesterday', 'today', 'me','notes here',2)
-  	// 	.subscribe(() => {
-  	// 		console.log('redirect will happen here')
-  			// here's the logic to redirect. when we move it out of ngOnInit, uncomment this.
-  			// this.router.navigate(['/template'])
-  		// })
   }
 
   ngAfterViewInit() {
     this.currEvent = this.eventsService.currEvent;
     console.log(this.currEvent)
     $("[id=timepicker-start]").pickatime({
-      default: this.item.start_time.slice(0,-3),
+      default: this.item.start_time,
       autoclose: true,
-      twelvehour: false
+      twelvehour: false,
     });
     $("[id=timepicker-end]").pickatime({
-      default: this.item.end_time.slice(0,-3),
+      default: this.item.end_time,
       autoclose: true,
-      twelvehour: false
+      twelvehour: false,
     });
   }
 
   updateEvent(name:string, start_time:string, end_time:string, default_instructor:string, notes:string, day_number:number) {
     console.log(arguments)
-    start_time = start_time.concat(":00");
-    end_time = end_time.concat(":00");
+
     let weekNum = Math.ceil(day_number/7);
     this.eventsService.updateEvent(this.eventsService.currEvent.event_id, name, start_time, end_time, default_instructor, notes, day_number)
       .subscribe(() => {
         console.log('updated')
-        // $('#modal1').closeModal();
         $(`#${this.item.event_id}`).closeModal();
         this.router.navigate(['/template', weekNum])
       })
   }
 
-  ngOnDestroy() {
-    // console.log('destroyed')
-    // this.item = {};
-  }
 }
